a18d7120c2da2012cfa77a995f33b6ba21ac3c6f
Eric Webber <ewebber283@gmail.com>
2022-09-03 17:35:31 -0500

    added ingredients jsx

--- src/App.js
+++ src/App.js
@@ -13,14 +13,38 @@ const testRecipes = [
     name: 'Chicken',
     servings: 4,
     cookTime: '30 minutes',
-    instructions: '1. Pre-heat oven to 375 \n2. Season chicken \n3. Put in oven'
+    instructions: '1. Pre-heat oven to 375 \n2. Season chicken \n3. Put in oven',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 tbsp'
+      }
+    ]
   },
   {
     id: 2,
     name: 'Pork',
     servings: 4,
     cookTime: '45 minutes',
-    instructions: '1. Pre-heat oven to 425 \n2. Season pork \n3. Put in oven'
+    instructions: '1. Pre-heat oven to 425 \n2. Season pork \n3. Put in oven',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 tbsp'
+      }
+    ]
   }
 ]
 export default App;
---
+++ src/IngredientList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Ingredient from './Ingredient'
+export default function IngredientList({ ingredients}) {
+    return (
+        <div>
+            {ingredients.map(ingredient => {
+                return (
+                    <Ingredient key={ingredient.id} 
+                    {...ingredient} 
+                    />
+                )
+            })}
+        </div>
+        
+      )
+}
